f98989623bb4450e8610b785886c3c27ba24d684
dogdduddy <dogdduddy@gmail.com>
2023-02-28 23:14:48 +0900

    feat : MVVM 패턴 도입
    Package mvvm 형태로 구현(model, view, viewModel)

--- app/src/main/java/com/dogdduddy/moviesearch/view/MainActivity.kt
+++ app/src/main/java/com/dogdduddy/moviesearch/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dogdduddy.moviesearch
+package com.dogdduddy.moviesearch.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dogdduddy.moviesearch.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
